23f5cf348dfa55480bbfafa47a3655e9fdbeac4d
Juan Pablo Velasquez <jvelasquez@bizlogit.net>
2024-05-30 01:09:02 -0300

    Conexión a MongoDB y agrego routes
    Queda hardcodeado el stringConnection

--- mi-api-restful/index.js
+++ mi-api-restful/index.js
@@ -1,22 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const cursoRoutes = require('./routes/cursos');
+const materiaRoutes = require('./routes/materias');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(bodyParser.json());
 
-// Ruta básica
 app.get('/', (req, res) => {
-  res.send('¡Hola, Mundo!');
+  res.send('Trabajo práctico: Arquitectura Web - UP');
 });
 
-// Iniciar el servidor
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://pablo:pablo@cluster0.vzeuaur.mongodb.net/?retryWrites=true&w=majority');
+    console.log('Conectado a la base de datos');
+  } catch (err) {
+    console.error('Error al conectar a la base de datos:', err);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+app.use('/api/cursos', cursoRoutes);
+app.use('/api/materias', materiaRoutes);
+
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
 
-const routes = require('./routes');
-app.use(routes);
 
